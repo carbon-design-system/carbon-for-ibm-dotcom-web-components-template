a52cf9ed54177eb628de86dacb5372bdaf9b1dfd
emyarod <afw5059@gmail.com>
2022-02-01 16:54:08 -0600

    chore: replace deprecated `babel-eslint-parser` with `@babel/eslint-parser`

--- packages/eslint-config-ibmdotcom/index.js
+++ packages/eslint-config-ibmdotcom/index.js
@@ -8,7 +8,7 @@
 'use strict';
 
 module.exports = {
-  parser: 'babel-eslint',
+  parser: '@babel/eslint-parser',
   extends: ['eslint:recommended', 'plugin:jsdoc/recommended'],
   plugins: ['jsdoc'],
   rules: {
